fdc15de2448c6f7b3780743c9104e43e8af6a8b8
JunWang <firetrio@gmail.com>
2020-04-30 14:55:02 +0900

    fix bug and rafactor and program

---
+++ GameConnect3/README.md
@@ -0,0 +1,28 @@
+# Game Connect 3 
+
+This mini project and all the source is from [this course](https://www.udemy.com/course/the-complete-android-oreo-developer-course/)
+
+## What I learned
+
+- Constraint Layout  
+- Grid Layout
+- View animate
+- Change view setting from java
+- Button on click function
+- Using `strings.xml` but not the hard string.
+etc.
+
+## Feeling/Remark
+
+It is difficult to control the components smoothly.
+
+## More function?
+
+Which I think is cool but I did not add into this project for now. (May be I will add them later)
+
+- Auto check the game is finished
+- Customize the color of button and select the color for player
+- Set which color is  first play
+- Set Play with PC
+- Record the history of play 
+- and so on
--- GameConnect3/app/src/main/java/com/example/gameconnect3/MainActivity.java
+++ GameConnect3/app/src/main/java/com/example/gameconnect3/MainActivity.java
@@ -2,28 +2,24 @@ package com.example.gameconnect3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.media.Image;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     // let 0:empty, 1:red, 2:yellow
     public int[] status = {0, 0, 0, 0, 0, 0, 0, 0, 0};
     public int[][] winPlan = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, {0, 4, 8}, {2, 4, 6}};
-
     public boolean isPlaying = false;
-
     public int playerNow = 0;
 
     public void dropIn(View view) {
 
+        // When game is started
         if (isPlaying) {
             ImageView imageview = (ImageView) view;
             int tappedViewIndex = Integer.parseInt(imageview.getTag().toString());
@@ -42,6 +38,8 @@ public class MainActivity extends AppCompatActivity {
                 }
                 imageview.animate().translationYBy(1500).setDuration(300);
 
+                // Whenever a empty place is clicked,
+                // the system will check whether game is over or not.
                 int winColor = checkWin(status, winPlan);
                 if (winColor != 0) {
                     TextView tv = (TextView) findViewById(R.id.textView);
@@ -58,14 +56,20 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private int checkWin(int[] status, int[][] winPlan) {
+    /**
+     * Using board status, check if winner is exist.
+     * @param status Status of board
+     * @param winPlans ALl the win status
+     * @return 0: No winner, 1: red win, 2: yellow win
+     */
+    private int checkWin(int[] status, int[][] winPlans) {
         int winColor = 0;
-        for (int[] plan : winPlan) {
-            int num1 = status[plan[0]];
-            if(num1 == 0) continue;
-            int num2 = status[plan[1]];
+        for (int[] winplan : winPlans) {
+            int num1 = status[winplan[0]];
+            if (num1 == 0) continue;
+            int num2 = status[winplan[1]];
             if (num1 != num2) continue;
-            int num3 = status[plan[2]];
+            int num3 = status[winplan[2]];
             if (num1 == num3) {
                 winColor = num1;
                 break;
@@ -74,41 +78,51 @@ public class MainActivity extends AppCompatActivity {
         return winColor;
     }
 
-    public void startClick(View view) {
-        Button botton = (Button) findViewById(R.id.button);
+    /**
+     * When the button is clicked, app will do
+     * @param view
+     */
+    public void buttonClick(View view) {
+        Button button = (Button) view;
         TextView textView = (TextView) findViewById(R.id.textView);
-        String bottonText = botton.getText().toString();
-        if (bottonText == getString(R.string.game_reset)){
+        String buttonText = button.getText().toString();
+        if (buttonText == getString(R.string.game_restart)) {
             resetGame(status);
-        }
-
-        if (!isPlaying) {
-            botton.setText(R.string.game_reset);
-            textView.setText(R.string.text_game_start);
-            isPlaying = true;
-        } else {
-            // restart or reset the game
-            textView.setText(R.string.text_game_reseted);
-            botton.setText(R.string.game_start);
+            button.setText(R.string.game_start);
             isPlaying = false;
+        } else if (buttonText == getString(R.string.game_start)) {
+
+            // if is not playing, start the game
+            if (!isPlaying) {
+                button.setText(R.string.game_restart);
+                textView.setText(R.string.text_game_start);
+                isPlaying = true;
+                Toast.makeText(getApplicationContext(), "Game Start!!", Toast.LENGTH_SHORT).show();
+            } else {
+                // game is started already, unexception throw
+                Toast.makeText(getApplicationContext(), "Unknown error", Toast.LENGTH_SHORT).show();
+            }
         }
     }
 
+    /**
+     * Clean the board and set the button to start
+     * @param status status of the board
+     */
     private void resetGame(int[] status) {
-        int[] idList = {R.id.iv1,R.id.iv2,R.id.iv3,R.id.iv4,R.id.iv5,R.id.iv6,R.id.iv7,R.id.iv8};
+        int[] idList = {R.id.iv1, R.id.iv2, R.id.iv3, R.id.iv4, R.id.iv5, R.id.iv6, R.id.iv7, R.id.iv8, R.id.iv9};
         for (int i = 0; i < 9; i++) {
             status[i] = 0;
             ImageView iv = (ImageView) findViewById(idList[i]);
             iv.setImageResource(0);
         }
+
         isPlaying = false;
         TextView tv = (TextView) findViewById(R.id.textView);
         Button b = (Button) findViewById(R.id.button);
         tv.setText(R.string.text_game_reseted);
         b.setText(R.string.game_start);
-
     }
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
